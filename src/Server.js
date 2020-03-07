97a54237a4d6ab13b77ae34c3bc3bcaa31f78fdd
Gabriel Gomes <bibiel2605@gmail.com>
2020-03-07 12:00:34 -0300

    backend almost complete

--- src/Server.js
+++ src/Server.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const routes = require('./routes')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
-// mongoose.connect('mongodb+srv://omnistack9:omnistack9@cluster0-lmjul.mongodb.net/omnistack9?retryWrites=true&w=majority', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// })
-
-mongoose.connect('mongodb://localhost:27017/omnistack9', {
+mongoose.connect('mongodb+srv://omnistack9:omnistack9@cluster0-lmjul.mongodb.net/omnistack9?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 
+// mongoose.connect('mongodb://localhost:27017/omnistack9', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
+
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
