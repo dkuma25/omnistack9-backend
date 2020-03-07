0af70af3d77f86aff2cb144ed7819888e3bcc390
Gabriel Gomes <bibiel2605@gmail.com>
2020-03-06 21:33:02 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Semana OmniStack 9.0 - Backend
+
+Backend da aplicação desenvolvida durante a Semana OmniStack 9.0.
+
+## Execução
+
+Executar backend em modo desenvolvedor:
+
+```
+yarn dev
+```
---
+++ src/Server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const routes = require('./routes')
+const mongoose = require('mongoose')
+
+// mongoose.connect('mongodb+srv://omnistack9:omnistack9@cluster0-lmjul.mongodb.net/omnistack9?retryWrites=true&w=majority', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
+
+mongoose.connect('mongodb://localhost:27017/omnistack9', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const app = express()
+
+app.use(express.json())
+app.use(routes)
+
+app.listen(3333)
