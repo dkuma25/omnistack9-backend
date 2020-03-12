3bf969185a9fe64c58cf76d39011677cd7c07056
Gabriel Gomes <bibiel2605@gmail.com>
2020-03-12 14:53:47 -0300

    static file path 'uploads'

--- src/Server.js
+++ src/Server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const routes = require('./routes')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const path = require('path')
 
 mongoose.connect('mongodb+srv://omnistack9:omnistack9@cluster0-lmjul.mongodb.net/omnistack9?retryWrites=true&w=majority', {
   useNewUrlParser: true,
@@ -17,6 +18,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes)
 
 app.listen(3333)
